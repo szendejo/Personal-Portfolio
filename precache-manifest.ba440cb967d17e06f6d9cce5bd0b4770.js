4a88776e9b92b605feaf24d8b7df3ed5f1d57023
Stephanie <szacademia@gmail.com>
2020-09-23 09:56:42 -0500

    Updates

---
+++ precache-manifest.ba440cb967d17e06f6d9cce5bd0b4770.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ba1816c91c980afd212fcc53cd09867",
+    "url": "/Personal-Portfolio/index.html"
+  },
+  {
+    "revision": "46bce1eaefc0cc71af63",
+    "url": "/Personal-Portfolio/static/css/main.08624717.chunk.css"
+  },
+  {
+    "revision": "f9ea0678edef0eb27db5",
+    "url": "/Personal-Portfolio/static/js/2.4d49c5bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Personal-Portfolio/static/js/2.4d49c5bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46bce1eaefc0cc71af63",
+    "url": "/Personal-Portfolio/static/js/main.ffe2145b.chunk.js"
+  },
+  {
+    "revision": "5f6272f9aeef81111bb0",
+    "url": "/Personal-Portfolio/static/js/runtime-main.50583493.js"
+  }
+]);
